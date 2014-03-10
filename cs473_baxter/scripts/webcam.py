1a9b439973e711440bae13120a7ed21bfb995978
Thomas Weng <thomas.weng11@gmail.com>
2014-03-10 03:15:49 -0400

    Add method to take snapshots from video stream.

--- cs473_baxter/scripts/webcam.py
+++ cs473_baxter/scripts/webcam.py
@@ -10,25 +10,51 @@ class Webcam():
 	def __init__(self):
 		self.device = 0 # assume we want first device
 		self.capture = cv2.VideoCapture(self.device)
-		self.capture.set(3,1280)
-		self.capture.set(4,1024)
+		self.capture.set(3,320)
+		self.capture.set(4,240)
 
-		# check if capture device is OK
 		if not self.capture:
 			print "Error opening capture device"
 			sys.exit(1)
 
-	def capture_frames(self):
-		while 1:
-			ret, img = self.capture.read()
-			cv2.imshow("input", img)
+	def show_video_stream(self):
+		while True:
+			val, frame = self.capture.read()
+			cv2.imshow("input", frame)
 
-			key = cv2.waitKey(10)
+			key = cv2.waitKey(50) % 256
+			if key == ord('q'):
+				break
+
+	def take_snapshots(self, delay=2):
+		print "press SPACE to take snapshots, press 'q' to quit."
+		take_picture = False;
+		t0, filenum = 0, 1
+
+		while True:
+			val, frame = self.capture.read()
+			cv2.imshow("video", frame)
+
+			key = cv2.waitKey(50) % 256
+			if key == ord(' '):
+				t0 = cv2.getTickCount()
+				take_picture = True
+			elif key == ord('q'):
+				break
+
+			if take_picture and ((cv2.getTickCount()-t0) / cv2.getTickFrequency()) > delay:
+				cv2.imwrite(str(filenum) + ".jpg", frame)
+				print "Image saved."
+				filenum += 1
+				take_picture = False
+
+			self.capture.release
 
 def main():
 	w = Webcam()
 
-	w.capture_frames()
+	#w.show_video_stream()
+	w.take_snapshots()
 
 if __name__ == '__main__':
 	main()
