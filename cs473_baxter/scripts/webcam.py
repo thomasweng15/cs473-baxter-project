d33ca10d05f3a4767df3f538d12b512275834e5e
Vance Zuo <vance.zuo@gmail.com>
2014-03-30 20:28:13 -0400

    Update webcam.py to take images of box, arm, etc.
    The method take_snapshots() was replaced with three methods, each for taking snapshots of reference pictures (background, box, arm), and pictures of the uncompressed/compressed object.

--- cs473_baxter/scripts/webcam.py
+++ cs473_baxter/scripts/webcam.py
@@ -13,8 +13,8 @@ class Webcam():
 		self.device = 0 # assume we want first device
 		print "Opening capture device..."
 		self.capture = cv2.VideoCapture(self.device)
-		self.capture.set(3,960)
-		self.capture.set(4,720)
+		self.capture.set(3,960) # CV_CAP_PROP_FRAME_WIDTH  
+		self.capture.set(4,720) # CV_CAP_PROP_FRAME_HEIGHT 
 		self.img_dir = img_dir
 
 		if not self.capture:
@@ -29,24 +29,41 @@ class Webcam():
 			key = cv2.waitKey(50) % 256
 			if key == ord('q'):
 				break
-
-	def take_snapshots(self):
-		print "Taking snapshot of view without object."
-		val, frame = self.capture.read()
-		cv2.imwrite(os.path.join(self.img_dir, "background.jpg"), frame)
-		print "Image saved."
-		print "Place object in center. Press SPACE when finished."
-
+			
+	def take_snapshot(self, file_name):
 		while True:
 			val, frame = self.capture.read()
 			cv2.imshow("input", frame)
 
 			key = cv2.waitKey(50) % 256
-			if key == ord(' '):
-				print "Taking snapshot of view with object."
-				cv2.imwrite(os.path.join(self.img_dir, "foreground.jpg"), frame)
+			if key == ord('q'):
+				print "Taking snapshot cancelled."
+				break
+			else if key == ord(' '):
+				print "Taking snapshot..."
+				cv2.imwrite(os.path.join(self.img_dir, file_name), frame)
 				print "Image saved."
 				break
+
+	def take_reference_snapshot(self):
+		print "Taking snapshot of background."
+		val, frame = self.capture.read()
+		cv2.imwrite(os.path.join(self.img_dir, "background.jpg"), frame)
+		print "Image saved."
+		
+		print "Place box in center. Press SPACE when finished."
+		self.take_snapshot("box.jpg")
+		
+		print "Remove box and move arm into view. Press SPACE when finished."
+		self.take_snapshot("arm.jpg")
+		
+	def take_uncompressed_snapshot(self):
+		print "Place object alone in center. Press SPACE when finished."
+		self.take_snapshot("object.jpg")
+		
+	def take_compressed_snapshot(self):
+		print "Compress object with arm. Press SPACE when finished."
+		self.take_snapshot("object.jpg")
 
 def main():
 	img_dir = "./src/cs473-baxter-project/cs473_baxter/images/"
